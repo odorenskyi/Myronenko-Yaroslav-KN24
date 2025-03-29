ca7852c32f84ccebfd2b230e2da54f09dc8a9dcd
Yaroslav Myronenko <jarik2707@gmail.com>
2025-03-29 17:21:22 +0200

    A test driver for the functions has been developed and the functions themselves have also been modified.

--- lab08/prj/ModulesMyronenko.h
+++ lab08/prj/ModulesMyronenko.h
@@ -1,10 +1,11 @@
 #ifndef MODULESMYRONENKO_H_INCLUDED
 #define MODULESMYRONENKO_H_INCLUDED
+#include <string>
 
 double s_calculation (double x, double y, double z); //Оголошення функції знаходження змінної S
 void Dev_inf (); //Оголошення функції виведення інформації про розробника
-string fujita_scale (double speed); // Оголошення функції для визначення категорії торнадо за шкалою Фудзіти
-void average_daily_temp (double temp[6]) // Оголошення функції для визначення середньодобової температури
-int count_bits (unsigned short N) // Оголошення функції для побітової обробки числа
+std::string fujita_scale (double speed); // Оголошення функції для визначення категорії торнадо за шкалою Фудзіти
+void average_daily_temp (); // Оголошення функції для визначення середньодобової температури
+int count_bits (unsigned short N); // Оголошення функції для побітової обробки числа
 
 #endif // MODULESMYRONENKO_H_INCLUDED

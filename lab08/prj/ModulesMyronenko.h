84b490217e09040c1dc5319109bfec5114a3ef10
Yaroslav Myronenko <jarik2707@gmail.com>
2025-04-10 19:30:24 +0300

    Functions added and declared in ModulesMyronenko.h

--- lab08/prj/ModulesMyronenko.h
+++ lab08/prj/ModulesMyronenko.h
@@ -7,5 +7,8 @@ void Dev_inf (); //Оголошення функції виведення інф
 std::string fujita_scale (double speed); // Оголошення функції для визначення категорії торнадо за шкалою Фудзіти
 void average_daily_temp (); // Оголошення функції для визначення середньодобової температури
 int count_bits (unsigned short N); // Оголошення функції для побітової обробки числа
+void task_10_1(const std::string& inputFile, const std::string& outputFile); // Функція для перевірки наявності слова у вірші та записує результати у вихідний файл
+void task_10_2(const std::string& inputFile, const std::string& outputFile); // Додає поточну дату й час запису до вихідного файлу
+void task_10_3(double x, double y, double z, int b, const std::string& outputFile); // Викликає функцію s_calculation та переводе число b в двійкове і записує все в вихідний файл
 
 #endif // MODULESMYRONENKO_H_INCLUDED
